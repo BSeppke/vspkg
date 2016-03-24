1156c12c1b6165129ec847048855cfb9dfc5cf07
Benjamin Seppke <benjamin.seppke@gmail.com>
2016-03-24 13:53:40 +0100

    re-ordered third party package settings

--- system/package_hooks.ps1
+++ system/package_hooks.ps1
@@ -1,10 +1,15 @@
-#----------------- SET ADDITIONAL PATHS FOR PERL, PYTHON AND QT--------------
+#----------------- ADDITIONAL PATHS FOR 3RD PARTY PACKAGES --------------------
+
 $VSP_PERL_PATH     = $VSP_INSTALL_PATH + "\perl"
 $VSP_PERL_UNIXPATH = $VSP_INSTALL_UNIXPATH + "/perl"
+add-to-envVar-if-necessary "$VSP_PERL_PATH\bin" "PATH"
 
 $VSP_PYTHON_PATH     = $VSP_INSTALL_PATH + "\python"
 $VSP_PYTHON_UNIXPATH = $VSP_INSTALL_UNIXPATH + "/python"
 $Env:PYTHONHOME      = $VSP_PYTHON_PATH
+add-to-envVar-if-necessary $VSP_PYTHON_PATH "PATH"
+add-to-envVar-if-necessary "$($VSP_PYTHON_PATH)\DLLs" "PATH"
+add-to-envVar-if-necessary "$($VSP_PYTHON_PATH)\Scripts" "PATH"
 
 $VSP_QT4_PATH     = $VSP_INSTALL_PATH + "\qt"
 $VSP_QT4_UNIXPATH = $VSP_INSTALL_UNIXPATH + "/qt"
@@ -12,14 +17,6 @@ $VSP_QT4_UNIXPATH = $VSP_INSTALL_UNIXPATH + "/qt"
 $VSP_QT5_PATH     = $VSP_INSTALL_PATH + "\qt5"
 $VSP_QT5_UNIXPATH = $VSP_INSTALL_UNIXPATH + "/qt5"
 
-#Switch for qt4 or qt5
+#Switch for qt4 or qt5 in PATH. You have to decide for ONE!
 $Env:QTDIR        = $VSP_QT5_PATH
-
-#------------------ ADD ADDITIONAL DIRECTORIES TO ENV -----------------------
-add-to-envVar-if-necessary "$VSP_PERL_PATH\bin" "PATH"
-
-add-to-envVar-if-necessary $VSP_PYTHON_PATH "PATH"
-add-to-envVar-if-necessary "$($VSP_PYTHON_PATH)\DLLs" "PATH"
-add-to-envVar-if-necessary "$($VSP_PYTHON_PATH)\Scripts" "PATH"
-
 add-to-envVar-if-necessary "$Env:QTDIR\bin" "PATH"
