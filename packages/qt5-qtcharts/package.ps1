d91069b6453b3cc6a98ccaacf80f3c96c09224ca
Benjamin Seppke <benjamin.seppke@gmail.com>
2016-07-21 09:28:32 +0200

    added package for qt5-charts, will replace qwt in most projects...

---
+++ packages/qt5-qtcharts/package.ps1
@@ -0,0 +1,77 @@
+param([switch]$force, [switch]$silent)
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+."..\..\system\tools.ps1" -silent
+."..\..\system\environment.ps1" -silent
+
+#------------------------------------------------------------------------------
+# STEP 1: CHECK, IF qt5-qtcharts IS ALREADY INSTALLED
+#------------------------------------------------------------------------------
+$logFile="$($VSP_INSTALL_REGISTRY_PATH)\qt5-qtcharts"
+if(test-path($logFile))
+{ 
+	if($force)
+	{
+		rm $logFile
+	}
+	else
+	{
+		if(-not $silent)
+		{
+			write-host "qwt has already been installed!" -Foreground Yellow
+			write-host "If you want to force installation, call this script again with the '-force' flag!" -Foreground Yellow
+		}
+		return
+	}
+}
+
+
+#------------------------------------------------------------------------------
+# STEP 2: INSTALL DEPENDENCIES
+#------------------------------------------------------------------------------
+..\qt5\package.ps1
+
+#------------------------------------------------------------------------------
+# STEP 3: INITIALIZE qt5-qtcharts
+#------------------------------------------------------------------------------
+cd $scriptPath
+if(test-path("$scriptPath\work"))
+{
+	rd work -force -recurse
+}
+md work >> $logFile
+cd work
+
+
+#------------------------------------------------------------------------------
+# STEP 4: FETCH qt5-qtcharts
+#------------------------------------------------------------------------------
+$src="https://github.com/qt/qtcharts/archive/v5.7.0.zip"
+$dest="$scriptPath\work\qtcharts-5.7.0.zip"
+download-check-unpack-file $src $dest "3AF4E15191724B9EF82E2991B3AD7C64"  >> $logFile
+
+#------------------------------------------------------------------------------
+# STEP 5: APPLY PATCHES TO qt5-qtcharts
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+# STEP 6: BUILD qt5-qtcharts 
+#------------------------------------------------------------------------------
+cd "qtcharts-5.7.0"
+
+&"$VSP_QT5_PATH\bin\qmake" >> $logFile
+nmake /NOLOGO >> $logFile
+
+
+#------------------------------------------------------------------------------
+# STEP 7: INSTALL qt5-qtcharts 
+#------------------------------------------------------------------------------
+nmake install /NOLOGO >> $logFile
+
+
+#------------------------------------------------------------------------------
+# STEP 7: CLEANUP qt5-qtcharts AND FINISH
+#------------------------------------------------------------------------------
+cd ..\..
+#rd work -force -recurse
+write-host "qt5-qtcharts has been installed successfully!" -Foreground Green
