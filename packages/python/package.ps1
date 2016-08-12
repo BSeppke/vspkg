ec7daf9e4ff06ff285065128466fe882599c9b6a
Benjamin Seppke <benjamin.seppke@gmail.com>
2016-08-12 17:00:10 +0200

    removed python3 package and introduces the following rule: If Visual Studio version is < 2015, install python 2.7.9. If Version >= 2015, install Python 3.5.2

--- packages/python/package.ps1
+++ packages/python/package.ps1
@@ -24,6 +24,14 @@ if(test-path($logFile))
 	}
 }
 
+#######
+#############
+##################### IF MSVC < 14 (2015) ----- USE PYTHON 2.7.9
+#############
+#######
+
+if($VSP_MSVC_VER -lt 14)
+{
 
 #------------------------------------------------------------------------------
 # STEP 2: INSTALL DEPENDENCIES
@@ -115,4 +123,102 @@ cp "$VSP_PYTHON_PATH\libs\*"    "$VSP_LIB_PATH" -force
 #------------------------------------------------------------------------------
 cd ..\..\..
 rd work -force -recurse
-write-host "python has been installed successfully!" -Foreground Green
+write-host "python 2.7.9 has been installed successfully!" -Foreground Green
+
+}
+else
+{
+#######
+#############
+##################### IF MSVC >= 14 (2015) ----- USE PYTHON 3.5.2
+#############
+#######
+
+#------------------------------------------------------------------------------
+# STEP 2: INSTALL DEPENDENCIES
+#------------------------------------------------------------------------------
+
+# The get_external.bat script is used herein. It needs to have svn in path!
+
+# Dependencies are already shipped with python:
+#   bzip2-1.0.6
+#   sqlite-3.8.11.0
+#   xz-5.0.5
+
+# Optional: If "%IncludeSSL%" is not "false".
+#  -> Necessary for downloads from https!
+#   nasm-2.11.06
+#   openssl-1.0.2h
+
+# Optional: If %IncludeTkinter% is not "false".
+# -> Not really necessary...
+#   tcl-core-8.6.4.2
+#   tk-8.6.4.2
+set-item "Env:\IncludeTkinter" "false"
+
+#------------------------------------------------------------------------------
+# STEP 3: INITIALIZE PYTHON
+#------------------------------------------------------------------------------
+cd $scriptPath
+if(test-path("$scriptPath\work"))
+{
+	rd work -force -recurse
+}
+md work >> $logFile
+cd work
+
+
+#------------------------------------------------------------------------------
+# STEP 4: FETCH PYTHON
+#------------------------------------------------------------------------------
+$src="https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz"
+$dest="$scriptPath\work\Python-3.5.2.tar.gz"
+download-check-unpack-file $src $dest "3fe8434643a78630c61c6464fe2e7e72" >> $logFile
+
+
+#------------------------------------------------------------------------------
+# STEP 5: APPLY PATCHES TO PYTHON
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# STEP 6: BUILD PYTHON 
+#------------------------------------------------------------------------------
+cd "Python-3.5.2\PCBuild"
+.\build.bat -e -p $VSP_BUILD_ARCH --no-tkinter >> $logFile
+
+#------------------------------------------------------------------------------
+# STEP 7: INSTALL PYTHON 
+#------------------------------------------------------------------------------
+create-directory-if-necessary "$VSP_PYTHON_PATH"
+create-directory-if-necessary "$VSP_PYTHON_PATH\include"
+create-directory-if-necessary "$VSP_PYTHON_PATH\DLLs"
+create-directory-if-necessary "$VSP_PYTHON_PATH\libs"
+
+$VSP_RELEASE_DIR = "Win32" 
+if($VSP_BUILD_ARCH -eq "x64")
+{
+	$VSP_RELEASE_DIR="amd64"
+}
+
+cp "$VSP_RELEASE_DIR\*.exe" "$VSP_PYTHON_PATH" -force
+cp "$VSP_RELEASE_DIR\*.dll" "$VSP_PYTHON_PATH" -force
+cp "$VSP_RELEASE_DIR\*.exp" "$VSP_PYTHON_PATH\libs" -force
+cp "$VSP_RELEASE_DIR\*.lib" "$VSP_PYTHON_PATH\libs" -force
+cp "$VSP_RELEASE_DIR\*.pyd" "$VSP_PYTHON_PATH\DLLs" -force
+
+cp "..\Include\*.h" "$VSP_PYTHON_PATH\include" -force
+cp "..\PC\pyconfig.h" "$VSP_PYTHON_PATH\include" -force
+
+cp "..\Lib" "$VSP_PYTHON_PATH" -recurse -force
+
+cp "$VSP_PYTHON_PATH\include\*" "$VSP_INCLUDE_PATH" -force
+cp "$VSP_PYTHON_PATH\libs\*"    "$VSP_LIB_PATH" -force
+
+
+#------------------------------------------------------------------------------
+# STEP 8: CLEANUP PYTHON AND FINISH
+#------------------------------------------------------------------------------
+cd ..\..\..
+rd work -force -recurse
+write-host "python 3.5.2 has been installed successfully!" -Foreground Green
+}
