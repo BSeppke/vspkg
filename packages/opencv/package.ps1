e8340f228c4e957a328c0d3a25a64890364ce0b5
Benjamin Seppke <benjamin.seppke@gmail.com>
2016-10-04 18:19:01 +0200

    moved to opencv 3.1

--- packages/opencv/package.ps1
+++ packages/opencv/package.ps1
@@ -53,21 +53,21 @@ cd work
 #------------------------------------------------------------------------------
 # STEP 4: FETCH OPENCV
 #------------------------------------------------------------------------------
-$src="https://codeload.github.com/Itseez/opencv/zip/2.4.11"
-$dest="$scriptPath\work\opencv-2.4.11.zip"
-download-check-unpack-file $src $dest "B517E83489C709EEE1D8BE76B16976A7" >> $logFile
+$src="https://codeload.github.com/Itseez/opencv/zip/3.1.0"
+$dest="$scriptPath\work\opencv-3.1.0.zip"
+download-check-unpack-file $src $dest "6082EE2124D4066581A7386972BFD52A" >> $logFile
 
 
 #------------------------------------------------------------------------------
 # STEP 5: APPLY PATCHES TO OPENCV (needed after build)
 #------------------------------------------------------------------------------
-unpack-file "..\opencv-2.4.11-patch.zip" "$VSP_INSTALL_PATH"  >> $logFile
+unpack-file "..\opencv-3.1.0-patch.zip" .  >> $logFile
 
 
 #------------------------------------------------------------------------------
 # STEP 6: BUILD OPENCV
 #------------------------------------------------------------------------------
-cd "opencv-2.4.11"
+cd "opencv-3.1.0"
 md build >> $logFile
 cd build
 
@@ -76,7 +76,7 @@ if ($VSP_BUILD_ARCH -eq "x64")
 {
 	$VSP_CMAKE_MSVC_GENERATOR = $VSP_CMAKE_MSVC_GENERATOR + " Win64"
 }
-&"$VSP_BIN_PATH\cmake.exe" "-G$VSP_CMAKE_MSVC_GENERATOR" "-Wno-dev" "-DCMAKE_INSTALL_PREFIX=$VSP_INSTALL_PATH" "-DCMAKE_PREFIX_PATH=$VSP_INSTALL_PATH" "-DCMAKE_BUILD_TYPE=Release" "-DWITH_OPENEXR=1" "-DWITH_QT=1" "-DWITH_JPEG=1" "-DWITH_PNG=1" "-DWITH_TIFF=1" "-DBUILD_opencv_python=1" "-DBUILD_SHARED_LIBS=1" ".." >> $logFile
+&"$VSP_BIN_PATH\cmake.exe" "-G$VSP_CMAKE_MSVC_GENERATOR" "-Wno-dev" "-DCMAKE_INSTALL_PREFIX=$VSP_INSTALL_PATH" "-DCMAKE_PREFIX_PATH=$VSP_INSTALL_PATH" "-DCMAKE_BUILD_TYPE=Release" "-DWITH_OPENEXR=1" "-DWITH_QT=1" "-DWITH_JPEG=1" "-DWITH_PNG=1" "-DWITH_TIFF=1" "-DBUILD_SHARED_LIBS=1" ".." >> $logFile
 
 devenv OpenCV.sln /Build "Release|$VSP_BUILD_ARCH" >> $logFile
 
@@ -98,12 +98,15 @@ $VSP_OPENCV_DIST_FOLDER = "$VSP_INSTALL_PATH\$VSP_OPENCV_DIST_FLAG\vc$VSP_MSVC_V
 
 cp "$VSP_OPENCV_DIST_FOLDER\bin\*" "$VSP_BIN_PATH" -force
 cp "$VSP_OPENCV_DIST_FOLDER\lib\*.lib" "$VSP_LIB_PATH"  -force
+cp "$VSP_OPENCV_DIST_FOLDER\lib\*.cmake" "$VSP_LIB_PATH"  -force
+cp "$VSP_OPENCV_DIST_FOLDER\staticlib\*.lib" "$VSP_LIB_PATH"  -force
 
 md "$VSP_SHARE_PATH\cmake\opencv" -force >> $logFile
 mv "$VSP_INSTALL_PATH\*.cmake" "$VSP_SHARE_PATH\cmake\opencv" -force
+
 #patch cmake files to new install dir
-cp "..\..\opencv-2.4.11-patch\share\cmake\opencv\*.cmake" "$VSP_SHARE_PATH\cmake\opencv" -force
-cp "..\..\opencv-2.4.11-patch\lib\*.cmake" "$VSP_LIB_PATH"  -force
+cp "..\..\opencv-3.1.0-patch\share\cmake\opencv\*.cmake" "$VSP_SHARE_PATH\cmake\opencv" -force
+cp "..\..\opencv-3.1.0-patch\lib\*.cmake" "$VSP_LIB_PATH"  -force
 
 md "$VSP_DOC_PATH\opencv" -force >> $logFile
 mv "$VSP_DOC_PATH\*.png" "$VSP_DOC_PATH\opencv" -force
--- system/config.ps1
+++ system/config.ps1
@@ -7,7 +7,7 @@ $VSP_BUILD_ARCH = "x64"
 #------------------- SET THE VERSION OF VISUAL STUDIO ------------------------
 # Allowed version: 10 (= 2010), 11 (= 2012), 12 (= 2013), 14 (= 2015)
 #$VSP_MSVC_VER = "10"
-#VSP_MSVC_VER = "11"
+#$VSP_MSVC_VER = "11"
 #$VSP_MSVC_VER = "12"
 $VSP_MSVC_VER = "14"
 
