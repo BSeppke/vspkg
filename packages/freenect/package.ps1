852c33b09bb4f535851fdd6397dc518a36b830ff
Benjamin Seppke <benjamin.seppke@gmail.com>
2016-10-05 10:22:35 +0200

    added patch for libfreenect to compile under visual studio 2015

--- packages/freenect/package.ps1
+++ packages/freenect/package.ps1
@@ -89,7 +89,7 @@ cp "lib\Release\*.dll"  "$VSP_BIN_PATH" -force
 cp "lib\Release\*.lib"  "$VSP_LIB_PATH" -force
 cp "lib\Release\*.exp"  "$VSP_LIB_PATH" -force
 
-md "$VSP_INCLUDE_PATH/libfreenect" >> $logFile
+New-Item -ItemType directory -Path "$VSP_INCLUDE_PATH/libfreenect" -Force >> $logFile
 cp "..\include\*.h" "$VSP_INCLUDE_PATH/libfreenect" -force
 cp "..\wrappers\c_sync\*.h" "$VSP_INCLUDE_PATH/libfreenect" -force
 
